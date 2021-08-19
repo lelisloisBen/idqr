ff1f68a4ca53150366a7142ec3bc7924d6d7e4d8
samir benzada <samirbenzada@samirs-MacBook-Pro.local>
2021-08-19 10:44:28 -0400

    installed qrcode and pillow

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from utils import APIException
 import os
+import qrcode
+from PIL import Image
 
 app = Flask(__name__)
 CORS(app)
@@ -10,6 +12,11 @@ CORS(app)
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
+
+@app.route('/test')
+def test():
+    img = qrcode.make('Your input text')
+    return img
 
 @app.route('/')
 def hello_world():
